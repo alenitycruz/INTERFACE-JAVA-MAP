525290094c9574227d867897e785213c46ee6307
alenitycruz <alessandradsmcruz@recife.pe.senac.br>
2022-02-26 12:45:18 -0300

    exercicios de map collections

---
+++ src/com/br/desafio/map/DesafioMap.java
@@ -0,0 +1,8 @@
+package com.br.desafio.map;
+/*
+-Faça um programa que simule um lançamento de dados.
+Lance o dado 100 vezes e armazene.
+Depois, mostre quantas vezes cada valor foi inserido.
+ */
+public class DesafioMap {
+}
---
+++ src/dio/java/collections/map/ordenacao/ExercicioProposto1.java
@@ -0,0 +1,25 @@
+package dio.java.collections.map.ordenacao;
+
+/*
+Dada a população estimada de alguns estados do NE brasileiros, faça:
+            Estado = PE - População = 9.616.621
+            Estado = AL - População = 3.351.543
+            Estado = CE - População = 9.187.103
+            Estado = RN - População = 3.534.265
+- Crie um dicionário e relacione os estados e suas populações;
+- Substitua a população do estado RN do por 3.534.165;
+- Confira se o estado da PB esta  dentro do dicionário, caso não adicione: PB - 4.039.277;
+- Exiba a população de PE;
+- Exiba todos os estados e suas populações na ordem que foi informado;
+- Exiba os estados e suas populações em ordem alfabética;
+- Exiba o estado com menor população e sua quantidade;
+- Exiba o estado com maior população e sua quantidade;
+- Exiba a soma da população desses estados;
+- Exiba a média da população deste dicionário de estados;
+- Remova os estados com a população menor que 4.000.000;
+- Apague o dicionário de estados;
+- Confira se o dicionário de estados está vazio.
+ */
+
+public class ExercicioProposto1 {
+}
